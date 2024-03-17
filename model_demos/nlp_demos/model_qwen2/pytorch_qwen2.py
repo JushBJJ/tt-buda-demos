9124af74d69e5d9d3399242fd6f6ca288805793b
JushBJJ <JushPacis@gmail.com>
2024-03-18 05:08:51 +1000

    fix: removed references to device and add extra compiler configs
    - Also removed unused imports

--- model_demos/nlp_demos/model_qwen2/pytorch_qwen2.py
+++ model_demos/nlp_demos/model_qwen2/pytorch_qwen2.py
@@ -1,12 +1,9 @@
-import torch
+import os
 import pybuda
 
 from pybuda._C.backend_api import BackendDevice
 from pybuda.transformers.pipeline import pipeline as pybuda_pipeline
-from transformers import Qwen2ForCausalLM, Qwen2Tokenizer, Qwen2Config, Qwen2Model
-
-# Use GPU if available, else CPU
-device = "cuda" if torch.cuda.is_available() else "cpu"
+from transformers import Qwen2ForCausalLM, Qwen2Tokenizer, Qwen2Config
 
 """
 === Models ===
@@ -24,7 +21,8 @@ model_name = "Qwen/Qwen1.5-0.5B"
 def run_qwen_causal_lm(max_length=512, top_p=0.9, top_k=50, temperature=0.7):
     # Set PyBuda configurations
     compiler_cfg = pybuda.config._get_global_compiler_config()
-    compiler_cfg.default_df_override = pybuda.DataFormat.Float16_b
+    compiler_cfg.default_df_override = pybuda._C.DataFormat.Float16_b
+    os.environ["TT_BACKEND_OVERLAY_MAX_EXTRA_BLOB_SIZE"] = "65536"
 
     # TODO Test on tonsterrent device
 
@@ -38,7 +36,7 @@ def run_qwen_causal_lm(max_length=512, top_p=0.9, top_k=50, temperature=0.7):
 
     # Load the model and tokenizer
     model = Qwen2ForCausalLM.from_pretrained(
-        model_name, config=config, device_map=device)
+        model_name, config=config)
     tokenizer = Qwen2Tokenizer.from_pretrained(model_name)
 
     tokenizer.pad_token = tokenizer.eos_token
